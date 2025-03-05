2061cd560c7c332ad40ab059f0eab0fb3dc37d9a
eht-ck <chandresh.beas@gmail.com>
2025-03-05 12:26:02 +0530

    feat: add product call

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -1,32 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
-// import './index.scss'
-import Header  from "./components/Header";
-import Footer  from "./components/Footer";
-import AnnouncementBanner from "./components/AnnouncementBanner";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home  from "./pages/Home";
+const Product = React.lazy(() => import("mf_product/Product"));
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Product from "mf_product/Product"
-import HeroSection from "./components/HeroSection";
-import About from "./components/About";
 const App = () => (
- <>
-    <AnnouncementBanner/>
-   <Header/>
-  <HeroSection/>
-<About/>
-   {/* <div>
-  <Product/>
-   </div> */}
-   <Footer/>
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
 
-   </>
- );
-const rootElement = document.getElementById("app")
-if (!rootElement) throw new Error("Failed to find the root element")
+const root = ReactDOM.createRoot(rootElement);
 
-const root = ReactDOM.createRoot(rootElement)
-
-root.render(<App />)
+root.render(<App />);
